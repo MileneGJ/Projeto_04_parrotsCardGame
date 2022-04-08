55f4d08f8c93458113e511ef0bf268f5a1116d9e
Milene <milene.jannetti@gmail.com>
2022-04-07 23:46:28 -0300

    Clicar nas cartas OK

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 let ncartas = prompt("Com quantas cartas quer jogar?");
 while (ncartas % 2 !== 0 || ncartas < 4 || ncartas > 14) {
-    alert(ncartas);
-    ncartas = prompt("Com quantas cartas quer jogar?");
+    ncartas = prompt("Escolha um número par entre 4 e 14!\nCom quantas cartas quer jogar?");
 }
 const figuras = ["bobrossparrot",
     "explodyparrot",
@@ -11,22 +10,22 @@ const figuras = ["bobrossparrot",
     "tripletsparrot",
     "revertitparrot"]
 
-    function comparador() {
+function comparador() {
     return Math.random() - 0.5;
 }
 
 figuras.sort(comparador);
 figurasEscolhidas = figuras.slice(0, (ncartas / 2));
-for(let i=0;i<(ncartas/2);i++){
-figurasEscolhidas.push(figurasEscolhidas[i]);
+for (let i = 0; i < (ncartas / 2); i++) {
+    figurasEscolhidas.push(figurasEscolhidas[i]);
 }
 figurasEscolhidas.sort(comparador);
 
 for (let i = 0; i < ncartas; i++) {
     let mesa = document.querySelector(".container")
-    mesa.innerHTML+= `<div class="carta" onclick="virarCarta(this)">
-    <img class="frente" src="./imagens/front.png">
-    <img class="tras" src="./imagens/${figurasEscolhidas[i]}.gif">
+    mesa.innerHTML += `<div class="carta" onclick="virarCarta(this)">
+    <div class="frente"><img src="./imagens/front.png"></div>
+    <div class="tras"><img src="./imagens/${figurasEscolhidas[i]}.gif"></div>
 </div>`;
 }
 
@@ -35,24 +34,24 @@ for (let i = 0; i < ncartas; i++) {
 function virada(element) {
     element.classList.toggle("virada")
     let listIMG = element.querySelectorAll("img");
-    let cont = 0;
-    while (cont < 2) {
-        listIMG[cont].classList.toggle("tras");
-        listIMG[cont].classList.toggle("frente");
-        cont++;
+    for (let i = 0; i < 2; i++) {
+        listIMG[i].parentNode.classList.toggle("tras");
+        listIMG[i].parentNode.classList.toggle("frente");
     }
 }
 
 function virarCarta(element) {
-    let cartaVirada = document.querySelector(".virada .frente");
+    let cartaVirada = document.querySelector(".virada > .frente > img");
     if (cartaVirada !== null) { cartaVirada = cartaVirada.src }
     virada(element);
-    let IMGcarta = element.querySelector(".frente").src;
+    let IMGcarta = element.querySelector(".frente > img").src;
     //Se já houver uma carta virada com img frente diferente dessa, então desvirar as duas
-    //A carta virada selecionada tem que ser sem par
     if (cartaVirada !== null && IMGcarta !== cartaVirada) {
-        alert("ERROU");
-        virada(element);
-        virada(document.querySelector(".virada"));
+        setTimeout(function(){virada(element)},1000);
+        setTimeout(function(){virada(document.querySelector(".virada"))},1000);
+    } else if (IMGcarta === cartaVirada) {
+        element.classList.remove("virada");
+        document.querySelector(".virada").classList.remove("virada");
     }
+
 }
